08cefd74d73ff6f79b22e33f0e1eedb9f5578d74
MateusSantosF <mateusferreira.dev@gmail.com>
2022-05-17 15:07:40 -0300

    Css in Home.html and fixed links in Courses.html

---
+++ src/scripts/loadScreen.js
@@ -0,0 +1,16 @@
+var timer
+var loadScreenDiv 
+var loadScreen
+document.addEventListener("DOMContentLoaded", function(e) {
+    
+   // timer = setTimeout('closeLoadScreen()',3000)  
+    loadScreenDiv = document.getElementById('loader-container')
+    loadScreen = document.getElementById('loader')
+    closeLoadScreen()
+});
+
+function closeLoadScreen(){
+    loadScreen.classList.toggle('sr-only')
+    loadScreenDiv.classList.toggle('loader-container')
+    loadScreenDiv.classList.toggle('sr-only')
+}
